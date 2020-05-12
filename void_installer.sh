9ec7fbd0c9c29763e2339618b22bc0088068d971
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2020-05-12 14:38:09 -0300

    Void Install for "Bridgewalker" System

--- void_installer.sh
+++ void_installer.sh
@@ -1,6 +1,6 @@
 sudo xbps-install -S void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 sudo xbps-install -Syv
-sudo xbps-install -S nano gcc make patch git curl xtools libopenal glm glfw firefox xorg-minimal xorg-fonts icewm nvidia pcmanfm audacity gimp obs ffmpeg setxkbmap menumaker
+sudo xbps-install -S vim gcc make patch git curl xtools libopenal glm glfw xorg-minimal xorg-fonts fvwm nvidia pcmanfm audacity gimp obs ffmpeg setxkbmap
 echo "Cloning void-packages."
 git clone https://github.com/void-linux/void-packages.git
 cd void-packages
@@ -16,10 +16,3 @@ sudo xbps-install -S nvidia-libs-32bit libdrm-32bit libgcc-32bit libstd++-32bit
 echo "Installing fun stuff."
 sudo xbps-install -S nethack fortune-mod cowsay
 cd ..
-echo ". $HOME/.bash_profile" >> .xinitrc
-echo "icewm" >> .xinitrc
-mmaker IceWM
-echo "[[ "$(tty)" = "/dev/tty1" ]] && fortune | cowsay" >> .bashrc
-echo "[[ "$(tty)" != "/dev/tty1" ]] && setxkbmap -layout br -variant abnt2 &" >> .bashrc
-echo "Use mmaker -h to get the WM you installed up and going with stuff."
-echo "You will also need to compile Kdenlive by yourself. Too outdated in xbps for your sane usage."
